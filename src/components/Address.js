5735bf4c16750225b950ab9a506404c0f2c2082a
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-10-12 15:15:20 +0000

    automatically backed up by learn

--- src/components/Address.js
+++ src/components/Address.js
@@ -1,1 +1,9 @@
 // Component Code Goes Here
+
+    class Address extends React.Component {
+      render() {
+        return (
+          <div className="address">{this.props.street}, {this.props.city}</div>
+        );
+      }
+    }
